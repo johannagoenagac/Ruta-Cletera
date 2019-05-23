9051d6880c81021eb3aad297c5d1d91987c8a127
johannagoenagac <johannagoenagac@gmail.com>
2019-05-23 09:23:35 -0300

    Updates

---
+++ precache-manifest.e18102626742373b9e4b5a6370e4d3c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c06a4803cda53a80bd64b69a62eefb0",
+    "url": "/Ruta-Cletera/index.html"
+  },
+  {
+    "revision": "af44239c3d93cef7da00",
+    "url": "/Ruta-Cletera/static/css/main.c11f6471.chunk.css"
+  },
+  {
+    "revision": "a683a8c1946865a78d09",
+    "url": "/Ruta-Cletera/static/js/2.9649e996.chunk.js"
+  },
+  {
+    "revision": "af44239c3d93cef7da00",
+    "url": "/Ruta-Cletera/static/js/main.fa35b2ed.chunk.js"
+  },
+  {
+    "revision": "abdd14a58981000fb1a8",
+    "url": "/Ruta-Cletera/static/js/runtime~main.ac44f26f.js"
+  }
+]);
